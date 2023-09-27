adb442e7b4788c85c3f4d0659c7ca3f6d0c604b3
Xxkirito974 <cloud.zf19@gmail.com>
2023-09-27 13:22:33 +0400

    ajout des fichiers au dépôt

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log("coucou")
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'youhou'
+});
+
+connection.connect();
+
+connection.connect((err)=> {
+    if (err) throw err
+    console.log("base de donnée connecté");
+});
