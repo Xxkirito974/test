8b20f3146f977c79c36b53e86f5b69a31eea95f4
Xxkirito974 <cloud.zf19@gmail.com>
2023-09-28 14:18:11 +0400

    mon depot

--- script.js
+++ script.js
@@ -1,14 +1,53 @@
-var mysql      = require('mysql');
-var connection = mysql.createConnection({
+import mysql from 'mysql';
+let connection = mysql.createConnection({
     host : 'localhost',
     user : 'root',
     password : '',
-    database : 'youhou'
+    database : 'youhou',
 });
 
-connection.connect();
+ connection.connect((err)=>{
+   if(err) throw err
+   console.log("ok")
+   getAllTasks();
+   getAlluser();
+   getTaskFromNameUser();
+  });
 
-connection.connect((err)=> {
-    if (err) throw err
-    console.log("base de donnée connecté");
-});
+  function getAllTasks() {
+    const selection = "SELECT * FROM tasks";
+    connection.query(selection, (err, result) => {
+        if(err) {
+            console.error("Essaie encore !!!", err);
+            return;
+        }
+        console.log("liste des taches");
+        console.log(result);
+    })
+  }
+
+  function getAlluser() {
+    const selection = "SELECT * FROM user";
+    connection.query(selection, (err, result) => {
+        if(err) {
+            console.error("Essaie encore !!!", err);
+            return;
+        }
+        console.log("liste des utilisateurs");
+        console.log(result);
+    })
+  }
+
+  
+  //getTaskFromNameUser 
+  function getTaskFromNameUser() {
+    const selection = "SELECT * FROM tasks INNER JOIN user ON user.id = tasks.owner_id";
+    connection.query(selection, (err, result) => {
+      if(err) {
+        console.error("Essaie encore !!!", err);
+        return;
+      }
+      console.log("Liste de tache par user");
+      console.log(result);
+    })
+  }
